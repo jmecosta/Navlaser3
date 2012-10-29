1cfa1476e73d1c54cc5c98bf7351273bb34731cb
jmecosta <jmecosta@gmail.com>
2012-10-29 22:12:52 +0200

    boost and qt

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -8,15 +8,9 @@ include (FindPkgConfig)
 
 ########################################
 
-# Set hardcoded path guesses for various platforms
-if (UNIX)
-  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} /usr/local /usr/local/lib/OGRE/cmake)
-  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} /usr/lib/OGRE/cmake)
-endif ()
-
 
 #### SET QT
 # find QT stuff - QTcore
 # find and setup Qt4 for this project
-FIND_PACKAGE(Qt4 REQUIRED)
+#FIND_PACKAGE(Qt4 REQUIRED)
 
